d27d6698eb161c78f998e0e8a85dd40028f89db1
Celeste-Rhoades <celesteisthebest82@gmail.com>
2025-02-24 12:00:37 -0700

    finished problem

--- A_arrays/string-to-lengths.js
+++ A_arrays/string-to-lengths.js
@@ -1,6 +1,10 @@
 // Write a function `stringsToLengths` that accepts an array of strings as an argument. The function
 // should return a new array containing the lengths of the elements of the original array.
-
+function stringsToLengths(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    return arr.length[i];
+  }
+}
 console.log(stringsToLengths(["belly", "echo", "irony", "pickled"]));
 // [5, 4, 5, 7]
 
