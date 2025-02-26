f211eeccae6be0444d912e72fd954891ff2a07df
Celeste-Rhoades <celesteisthebest82@gmail.com>
2025-02-26 08:23:23 -0700

    filter word length

--- A_arrays/filter-long-words.js
+++ A_arrays/filter-long-words.js
@@ -1,6 +1,16 @@
 // Write a function `filterLongWords` that accepts an array of strings as an argument. The function
 // should return a new array containing only the strings that are less than 5 characters long.
 
+function filterLongWords(arr) {
+  let words = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length < 5) {
+      words.push(arr[i]);
+    }
+  }
+  return words;
+}
+
 console.log(filterLongWords(["kale", "cat", "retro", "axe", "heirloom"]));
 // ['kale', 'cat', 'axe']
 
