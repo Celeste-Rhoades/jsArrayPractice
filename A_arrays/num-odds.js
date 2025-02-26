475401af52c0ed231612ebbc3943f2f04630de9b
Celeste-Rhoades <celesteisthebest82@gmail.com>
2025-02-26 11:56:33 -0700

    odd num

--- A_arrays/num-odds.js
+++ A_arrays/num-odds.js
@@ -1,5 +1,14 @@
 // Write a function `numOdds` that accepts an array of numbers as an argument. The function should
 // return a number representing the count of odd elements in the array.
+function numOdds(nums) {
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 === 1) {
+      count++;
+    }
+  }
+  return count;
+}
 
 console.log(numOdds([4, 7, 2, 5, 9])); // 3
 console.log(numOdds([11, 31, 58, 99, 21, 60])); // 4
