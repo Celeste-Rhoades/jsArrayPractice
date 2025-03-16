50d8b40e7e9204a54f0e0685bc0e42f65e8dd359
Celeste-Rhoades <celesteisthebest82@gmail.com>
2025-03-16 14:36:53 -0600

    chooseDivisible

--- B_Arrays/chooseDivisibles.js
+++ B_Arrays/chooseDivisibles.js
@@ -2,6 +2,17 @@
 // target number as arguments. The function should return an array containing elements of the original
 // array that are divisible by the target.
 
+function chooseDivisibles(numbers, target) {
+  let divisible = [];
+  for (let i = 0; i < numbers.length; i++) {
+    let nums = numbers[i];
+    if (nums % target === 0) {
+      divisible.push(numbers[i]);
+    }
+  }
+  return divisible;
+}
+
 console.log(chooseDivisibles([40, 7, 22, 20, 24], 4)); // [40, 20, 24]
 console.log(chooseDivisibles([9, 33, 8, 17], 3)); // [9, 33]
 console.log(chooseDivisibles([4, 25, 1000], 10)); // [1000]
