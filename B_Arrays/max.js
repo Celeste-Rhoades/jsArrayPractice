541f15dbe994f66be99ae18b0dcf8695c9410661
Celeste-Rhoades <celesteisthebest82@gmail.com>
2025-03-16 15:19:33 -0600

    max

--- B_Arrays/max.js
+++ B_Arrays/max.js
@@ -1,6 +1,17 @@
 // Write a function `maximum` that accepts an array of numbers as an argument. The function should
 // return the largest number of the array. If the array is empty, then the function should return null.
 
+function maximum(nums) {
+  if (nums.length === 0) {
+    return null;
+  }
+  let largest = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    if (largest < nums[i]) largest = nums[i];
+  }
+  return largest;
+}
+
 console.log(maximum([5, 6, 3, 7])); // 7
 console.log(maximum([17, 15, 19, 11, 2])); // 19
 console.log(maximum([])); // null
