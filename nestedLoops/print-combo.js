7305158929d8c5f82a97128bd40e2ebca15682e7
Celeste-Rhoades <celesteisthebest82@gmail.com>
2025-03-18 15:42:07 -0600

    print-combo

--- nestedLoops/print-combo.js
+++ nestedLoops/print-combo.js
@@ -3,6 +3,15 @@
 // and an element from the second array. The function doesn't need to return any value. It
 // should just print to the terminal.
 
+function printCombinations(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      let pair = [arr1[i], arr2[j]];
+      console.log(pair);
+    }
+  }
+}
+
 let colors = ["gray", "cream", "cyan"];
 let clothes = ["shirt", "flannel"];
 printCombinations(colors, clothes);
