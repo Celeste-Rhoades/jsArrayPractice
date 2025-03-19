c03c34ba679d5abe7f06618568cea39b4f63e1ba
Celeste-Rhoades <celesteisthebest82@gmail.com>
2025-03-19 14:43:00 -0600

    total product

--- 2DArray/totalProduct.js
+++ 2DArray/totalProduct.js
@@ -1,6 +1,17 @@
 // Write a function `totalProduct(array)` that accepts a 2D array of numbers. The function should return
 // the total product of all numbers in the array.
 
+function totalProduct(array) {
+  let product = 1;
+  for (let i = 0; i < array.length; i++) {
+    let subarr = array[i];
+    for (let j = 0; j < subarr.length; j++) {
+      product *= subarr[j];
+    }
+  }
+  return product;
+}
+
 let array1 = [
   [3, 5, 2],
   [6, 2],
