ed53b04f742428a5c328adddf713254df0fbfa21
Celeste-Rhoades <celesteisthebest82@gmail.com>
2025-03-19 12:09:58 -0600

    two-sum

--- nestedLoops/pairPrint.js
+++ nestedLoops/pairPrint.js
@@ -4,7 +4,7 @@
 function pairPrint(arr) {
   for (let i = 0; i < arr.length; i++) {
     for (let j = i + 1; j < arr.length; j++) {
-      console.log(arr[i], arr[j]);
+      console.log(arr[i] + " - " + arr[j]);
     }
   }
 }
