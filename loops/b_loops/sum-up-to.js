b008af49f099f7fb7bcf58225ae427f50d4da518
Celeste-Rhoades <celesteisthebest82@gmail.com>
2025-03-29 15:18:24 -0600

    sum up to

--- loops/b_loops/sum-up-to.js
+++ loops/b_loops/sum-up-to.js
@@ -3,6 +3,14 @@
 //
 // For example, sumUpTo(4) should return 10 because 1 + 2 + 3 + 4 = 10.
 
+function sumUpTo(max) {
+  let sum = 0;
+  for (let i = 1; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
 console.log(sumUpTo(4)); // 10
 console.log(sumUpTo(5)); // 15
 console.log(sumUpTo(2)); // 3
