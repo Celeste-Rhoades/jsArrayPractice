1f2aff21c191734d041c7d6ae3bc56ba0b3f626e
Celeste-Rhoades <celesteisthebest82@gmail.com>
2025-03-30 11:40:16 -0600

    no ohs

--- loops/b_loops/no-ohs.js
+++ loops/b_loops/no-ohs.js
@@ -1,6 +1,26 @@
 // Write a function named `noOhs` that accepts a string as an argument. The functions should print the
 // characters of the string one by one except the character 'o'. The function doesn't need to return
 // any value. It should just print to the terminal.
+function noOhs(str) {
+  let newStr = "";
+  for (let i = 0; i < str.length; i++) {
+    if (str[i].replace("o", "")) {
+      newStr += str[i];
+    }
+  }
+  console.log(newStr);
+}
+
+or; //
+function noOhs(str) {
+  let newStr = "";
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (char !== "0") {
+      console.log(newStr);
+    }
+  }
+}
 
 noOhs("code");
 // prints
