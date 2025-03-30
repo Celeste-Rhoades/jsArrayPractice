d38e52e4e059bc2145dd2f471bdfabadbbeb5022
Celeste-Rhoades <celesteisthebest82@gmail.com>
2025-03-30 12:09:14 -0600

    odd-sum

--- loops/b_loops/odd-sum.js
+++ loops/b_loops/odd-sum.js
@@ -3,5 +3,14 @@
 //
 // For example, oddSum(10) should return 25 because 1 + 3 + 5 + 7 + 9 = 25
 
+const oddSum = max => {
+  let num = 0;
+  for (let i = 1; i <= max; i++) {
+    if (i % 2 === 1) {
+      num += i;
+    }
+  }
+  return num;
+};
 console.log(oddSum(10)); // 25
 console.log(oddSum(5)); // 9
